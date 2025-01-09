b2a67890dd9585c70d452a5a3013caaa75ea88c3
PC-THOMAS\thoma <thomas.colomba30>
2025-01-09 14:58:11 +0100

    arranca un tomcat pre-instalado con el CATALINA_BASE siendo la raíz del proyecto, dónde está el .bat

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # tomcat_9_catalina_base_configuration_2
+
+eso arranca el tomcat usando un CATALINA_BASE distinto de su valor per defecto, que en ese caso es la carpeta del .bat
+
---
+++ bin/setenv.bat
@@ -0,0 +1,6 @@
+REM script escrito por Thomas : defino el jdk y la carpeta base dónde se leerán los archivos de config del tomcat al ejecutarlo
+
+REM debug set "JRE_HOME=C:\java\jdk-11.0.24\bin\"
+set "JRE_HOME=C:\java\jdk8\jre"
+
+exit /b 0
